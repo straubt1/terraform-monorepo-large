d80cf5b60f32a2bc44e1b040e9aff2c34136060d
straubt1 <tstraub@hashicorp.com>
2024-11-18 11:07:00 -0600

    Main

---
+++ terraform-05/main.tf
@@ -0,0 +1,17 @@
+variable "test_var" {
+  description = "Dummy variable that can be used to force a variable error."
+  type        = string
+  default     = 10
+}
+
+# Generate a change in the workspace
+resource "random_pet" "main" {
+  keepers = {
+    always = timestamp()
+  }
+  length = var.test_var
+}
+
+output "pet_name" {
+  value = random_pet.main.id
+}
